17ed4f4cdc97fdbfc39d7314a10d2a41f31904a2
Omar Aguirre <omar.fi.wwr@gmail.com>
2025-04-17 13:55:16 -0600

    Initialized VUE Project

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    mensaje: "Hola desde Vuex 😎",
+  },
+  mutations: {},
+  actions: {},
+  modules: {},
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container mt-5">
+    <h1>Bienvenido a tu app Vue con Bootstrap 🚀</h1>
+  </div>
+</template>
+
+<script setup></script>
